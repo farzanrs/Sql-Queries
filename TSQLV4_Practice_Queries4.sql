5eba38f5b372d439d9b4642978381e2ef85a3d2b
Farzan Rahbar <rahbarfarzan@gmail.com>
2026-08-11 23:46:14 +0330

    Add files via upload
     `Implement Transaction Isolation levels and Locking mechanisms (Snapshot & Serializable)`

---
+++ TSQLV4_Practice_Queries4.sql
@@ -0,0 +1,26 @@
+﻿ALTER PROCEDURE sp_LockCustomers
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+    BEGIN TRANSACTION;
+        -- استفاده از XLOCK برای گرفتن قفل انحصاری
+        SELECT *  FROM Sales.Customers WITH (XLOCK);
+        WAITFOR DELAY '00:00:30'; 
+    COMMIT TRANSACTION;
+END;
+go
+
+CREATE PROCEDURE sp_GetLastCommittedOrders
+AS
+BEGIN
+    -- تنظیم لول ایزولاسیون روی SNAPSHOT 
+    -- این لول باعث می‌شود آخرین نسخه Commit شده (صحیح) را ببینیم
+    SET TRANSACTION ISOLATION LEVEL SNAPSHOT;
+
+    BEGIN TRANSACTION;
+        SELECT * FROM Sales.Orders;
+    COMMIT TRANSACTION;
+END;
+
+
